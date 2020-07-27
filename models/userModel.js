b39508a2b94a197cd6d652e4673efc2a02f7d220
yurii <yuriy.tim@yahoo.com>
2020-07-26 20:10:50 -0400

    to pass token to the user

--- models/userModel.js
+++ models/userModel.js
@@ -21,6 +21,7 @@ let userSchema = new mongoose.Schema({
     required: [true, 'Please provide a password'],
     minlength: 8,
     trim: true,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -45,6 +46,13 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('Users', userSchema);
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
 router.post('/signup', authController.singup);
+router.post('/login', authController.login);
 
 router
   .route('/')
